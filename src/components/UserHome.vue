f478821f843705ec1188bdbbabe5cfb403a4641b
sadjo L. sarai <sadjosarai@gmail.com>
2022-10-16 10:04:12 +0100

    component slot

---
+++ src/components/UserHome.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="window">
+        <slot>
+            <h1>
+                ERROR 404
+            </h1>
+            <h2>
+                content not found
+            </h2>
+        </slot>
+    </div>
+</template>
+<style scoped>
+    .window{
+        height:50vh;
+        width:50vw;
+        display:flex;
+        flex-flow:column wrap;
+        align-items: center;
+        border:1px solid grey;
+        border-radius:5px;
+        position:relative;
+    }
+   
+</style>
