8d736d044c709efeb421452f2ac1b86cfbb49263
sarai sadjo legbane <sarai.sadjo@facsciences-uy1.cm>
2023-09-11 19:55:18 +0200

    update, to enlight the userhome component on App.vue

--- src/components/TodoDock.vue
+++ src/components/TodoDock.vue
@@ -23,7 +23,8 @@ export default {
 </script>
 <style scoped>
     .dock-container{
-        background-color: rgb(202, 192, 179);
+        background-color: rgb(185, 224, 243);
+        backdrop-filter: blur(50px);
         width:250px;
         height: auto;
         max-height: 90vh;
@@ -31,13 +32,14 @@ export default {
         flex-direction: column;
         justify-content: start;
         align-items: center;
-        border-radius:10px 10px 0px 0px;
+        border-radius:10px 10px 10px 10px;
         padding-bottom:10px;
     }
     .dock-container:hover{
-        background-color: rgb(200, 190, 180);;
         color:black;
-        box-shadow: 0px 0px 1px 1px rgba(128, 128, 128, 0.445);
+        box-shadow: 0px 0px 2px 2px rgb(209, 232, 243);
+        border: 1px solid rgb(20, 79, 109);
+        transition: ease;
     }
     .dock-header{
         display: flex;
@@ -66,7 +68,6 @@ export default {
     }
     .dock-body{
         width:calc(95% - 20px);
-        background-color: beige;
         color:grey;
         padding:0px 10px;
     }
